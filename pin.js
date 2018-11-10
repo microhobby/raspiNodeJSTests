b46a2b5c9f8eb4eacd5e7e3177773257c02235c0
Matheus Castello <matheus@castello.eng.br>
2018-11-10 15:01:46 -0200

    Pin: Implement interrupt inputs
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- pin.js
+++ pin.js
@@ -1,7 +1,7 @@
 
 const Gpio = require("onoff").Gpio;
 
-exports.Pin = function(pin) {
+exports.Pin = function(pin, edge, debounce) {
 	const pinNumber = pin;
 	var gpioObject = null;
 
@@ -11,7 +11,13 @@ exports.Pin = function(pin) {
 	this.blinkInterval = { id: 0 };
 
 	this.mode = function(direction) {
-		gpioObject = new Gpio(pinNumber, direction);
+		if (!edge && !debounce)
+			gpioObject = new Gpio(pinNumber, direction);
+		else if(!debounce)
+			gpioObject = new Gpio(pinNumber, direction, edge);
+		else
+			gpioObject = new Gpio(pinNumber, direction, edge,
+					{debounceTimeout: debounce});
 	};
 
 	this.set = function() {
@@ -25,6 +31,47 @@ exports.Pin = function(pin) {
 	this.toggle = function() {
 		var pinVal = gpioObject.readSync();
 		gpioObject.writeSync(!pinVal ? 1 : 0);
+	};
+
+	this.onClick = function(callBack) {
+		var pin = this;
+	
+		gpioObject.watch(function(err, value) {
+			if (callBack)
+				callBack();
+		});
+	};
+
+	this.onChange = function(callBack) {
+		var pin = this;
+	
+		gpioObject.watch(function(err, value) {
+			if (callBack)
+				callBack();
+		});
+	};
+
+	this.startBlink = function(time) {
+		var pin = this;
+
+		console.log("ID::" + pin.blinkInterval.id);
+
+		/* clear previous */
+		if (pin.blinkInterval.id != 0) {
+			clearInterval(pin.blinkInterval.id);
+			pin.blinkInterval.id = 0;
+		}
+
+		pin.blinkInterval.id = setInterval(function() {
+			pin.toggle();
+		}, time);
+	};
+
+	this.stopBlink = function() {
+		if (this.blinkInterval.id != 0) {
+			clearInterval(this.blinkInterval.id);
+			this.blinkInterval.id = 0;
+		}
 	};
 
 	this.blinkXTimes = function(times, onEnd) {
