320ad54a047157b50420dc6ef16bd14e90700588
Matheus Castello <matheus@castello.eng.br>
2018-11-13 16:02:28 -0200

    GPIO: true and false thing
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- output.js
+++ output.js
@@ -4,5 +4,6 @@ const Gpio = require("onoff").Gpio;
 var pin = new Gpio(17, "out");
 
 setInterval(function() {
-	pin.writeSync(!pin.readSync());
+	pin.writeSync((pin.readSync() ? 0 : 1));
 }, 200);
+
